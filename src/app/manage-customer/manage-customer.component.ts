791917003ece1a9c4e8a1b9afef66388dbc1ed51
Suchira Liyanage <suchiraliyanage94@gmail.com>
2020-12-15 07:18:02 +0530

    Change the manage-customer form

--- src/app/manage-customer/manage-customer.component.ts
+++ src/app/manage-customer/manage-customer.component.ts
@@ -2,6 +2,11 @@
 import manageCustomer from './manage-customer.component.html';
 import style from './manage-customer.component.scss';
 
+import '../../../node_modules/admin-lte/plugins/datatables/jquery.dataTables.min.js';
+import '../../../node_modules/admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js';
+import '../../../node_modules/admin-lte/plugins/datatables-responsive/js/dataTables.responsive.min.js';
+import '../../../node_modules/admin-lte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js';
+
 $("app-manage-customer").replaceWith('<div id="manage-customer" class="d-none">' + manageCustomer + '</div>');
 var html = '<style>' + style + '</style>';
 $("#manage-customer").append(html);
